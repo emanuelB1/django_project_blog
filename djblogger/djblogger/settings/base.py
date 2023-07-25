3d0a498ee4a5eef82baafa38e02bc4c80e2bdc2e
emanuelB1 <128659205+emanuelB1@users.noreply.github.com>
2023-07-25 13:09:43 -0400

    save

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -7,15 +7,13 @@ from djblogger.logging import *
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-
 
 ALLOWED_HOSTS = ['*']
 
