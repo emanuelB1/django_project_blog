1bd6ba38c4a093703bd6d03fd723954065ff9ed3
emanuelB1 <128659205+emanuelB1@users.noreply.github.com>
2023-07-25 14:30:20 -0400

    save

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'imagekit',
     'django.contrib.sitemaps',
+    'apps.core',
     
 
     
--- djblogger/djblogger/settings/production.py
+++ djblogger/djblogger/settings/production.py
@@ -1,1 +1,30 @@
-from .base import *
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from djblogger.logging import *
+from djblogger.settings.base import *
+
+load_dotenv(Path.joinpath(BASE_DIR, '.env'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
+
+
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles/')
+
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
